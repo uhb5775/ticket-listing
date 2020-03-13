23d924a39ac36cf98526d9e9d834831a4f5a15d7
enzzure <uhb5775@gmail.com>
2020-03-13 10:39:36 +0200

    5-fix-counting

--- app/Http/Controllers/LocationsController.php
+++ app/Http/Controllers/LocationsController.php
@@ -91,15 +91,14 @@ class LocationsController extends Controller
     public function wallet($id) //location/id/up
     {
         $sales = Location::find($id)->salesz()->get();
-        // $res = Wallet::where('added_to_drawer', '=', '0')->delete();
 
         $locations = Location::find($id)->orders()
-        ->where('added_to_drawer', 1)
-        ->whereDate('created_at', '=', Carbon::today()->toDateString()); //show orders
+        ->where('added_to_drawer', 1);
+        // ->whereDate('created_at', '=', Carbon::today()->toDateString()); //show orders
         
         $wallets = Location::find($id)->locsales()
         ->where('added_to_drawer', 1)
-        ->whereDate('created_at', '=', Carbon::today()->toDateString())
+        // ->whereDate('created_at', '=', Carbon::today()->toDateString())
         ->get(); //show payments
         $loc = Location::find($id); //to show location name
 
@@ -149,8 +148,8 @@ class LocationsController extends Controller
             ]);    
         // $wallet = Sales::find($id);
         $wallet = new Sales();
-        $resetOrders = Location::find($id)->orders()->update(array('added_to_drawer' => 0));
-        $resetPayments = Location::find($id)->locsales()->where('added_to_drawer', '=', '1')->delete();
+        $resetOrders = Location::find($id)->orders()->update(array('added_to_drawer' => 0)); //reset orders counting
+        $resetPayments = Location::find($id)->locsales()->where('added_to_drawer', '=', '1')->delete(); //reset payments counting
 
         $wallet->location_id = $request->input('location_id');
         $wallet->start_cash = $request->input('start_cash');
